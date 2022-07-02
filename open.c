07338391689a8315320c9edfddb95ab16d82315f
Jim Blandy <jimb@red-bean.com>
2022-07-02 16:07:31 -0700

    count context switches

--- open.c
+++ open.c
@@ -1,6 +1,7 @@
 #include <linux/hw_breakpoint.h> /* Definition of HW_* constants */
 #include <linux/perf_event.h>    /* Definition of PERF_* constants */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/syscall.h>         /* Definition of SYS_* constants */
 #include <unistd.h>
@@ -17,11 +18,15 @@ int main(int argc, char **argv) {
   attrs.exclude_kernel = 1;
   attrs.exclude_hv = 1;
   attrs.exclude_idle = 1;
+  attrs.type = PERF_TYPE_SOFTWARE;
+  attrs.config = PERF_COUNT_SW_CONTEXT_SWITCHES;
+
   int status = perf_event_open(&attrs, 0, -1, -1, PERF_FLAG_FD_NO_GROUP);
-  if (status > 0) {
-    printf("success: %d", status);
-  } else {
+  if (status < 0) {
     perror("perf_event_open failed");
+    exit(1);
   }
+
+  printf("success: %d", status);
   return 0;
 }
